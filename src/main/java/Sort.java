256f6ed032ed4f55c2499ab709aa0541ea121661
cal watson <jcwatso2000@yahoo.com>
2012-07-14 08:49:55 -0400

    Sort Interface

---
+++ src/main/java/Sort.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: jcwatso
+ * Date: 7/14/12
+ * Time: 8:49 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Sort {
+}
